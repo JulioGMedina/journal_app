279bb7c6edbe8b3cf70fa0a3b38a29c8c07dea73
Julio Medina <julio@webtoran.com>
2016-07-16 16:55:33 -0400

    updated devise form styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def error_messages
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+      <div class="row">
+        <div class="small-12 columns text-center">
+          <h2>#{sentence} !!!!!</h2>
+          <ul>#{messages}</ul>
+        </div>
+      </div>
+    HTML
+
+    html.html_safe
+  end
 end
